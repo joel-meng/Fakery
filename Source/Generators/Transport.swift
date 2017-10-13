025a0e0f2257fc2ff0de2cd8378021b6e97b1640
Joel Meng <joel.mong.au@gmail.com>
2017-10-13 16:18:45 +1000

    Update Transport, added stop

--- Source/Generators/Transport.swift
+++ Source/Generators/Transport.swift
@@ -11,22 +11,34 @@ import Foundation
 public final class Transport: Generator {
   
   public func busShortName() -> String {
-    return numerify(generate("transport.bus_shortName"))
+    return numerify(generate("transport.bus_short_name"))
   }
   
   public func busLongName() -> String {
-    return generate("transport.bus_longName")
+    return generate("transport.bus_long_name")
   }
   
   public func trainShortName() -> String {
-    return letterify(generate("transport.train_shortName"))
+    return letterify(generate("transport.train_short_name"))
   }
   
   public func trainLongName() -> String {
-    return generate("transport.train_longName")
+    return generate("transport.train_long_name")
   }
   
   public func trainLine() -> String {
     return generate("transport.train_line")
+  }
+  
+  public func busStopName() -> String {
+    return bothify(generate("transport.bus_stop_name"))
+  }
+  
+  public func trainStationName() -> String {
+    return numerify(generate("transport.train_station_name"))
+  }
+  
+  public func agencyName() -> String {
+    return generate("transport.agency_name")
   }
 }
--- Tests/Fakery/Generators/TransportSpec.swift
+++ Tests/Fakery/Generators/TransportSpec.swift
@@ -22,12 +22,12 @@ class TransportSpec: QuickSpec {
         transport = Transport(parser: parser)
       }
       
-      it("returns correct text", closure: {
+      it("returns correct bus short name", closure: {
         let valueUT = transport.busShortName()
         expect(valueUT).to(match("^\\d{3}$"))
       })
 
-      it("returns correct text", closure: {
+      it("returns correct bus long name", closure: {
         let valueUT = transport.busLongName()
         expect(valueUT).to(equal("Kangaroo Point - Kangaroo Point"))
       })
@@ -47,6 +47,17 @@ class TransportSpec: QuickSpec {
         let valueUT = transport.trainLine()
         expect(valueUT).to(equal("Kangaroo Point line"))
       })
+      
+      it("returns correct bus stop name", closure: {
+        let valueUT = transport.busStopName()
+        expect(valueUT).to(match("^Vadym Avenue at Kangaroo Point, stop \\d{2}$"))
+      })
+      
+      it("returns correct train staion name", closure: {
+        let valueUT = transport.trainStationName()
+        expect(valueUT).to(match("^Kangaroo Point, platform \\d{1}"))
+      })
+
     }
   }
 }
